7d3359c280ca406ea03e7c6850dd2d947e11e6a9
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-09-24 20:07:37 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -35,5 +35,6 @@ function render() {
 };
 
 let store = createStore(candyReducer)
+dispatch({type: "default"})
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
